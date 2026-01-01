f9d68638d20192c3b6577a67bdda8cef07652b36
Wouter Verweirder <wouter.verweirder@gmail.com>
2026-01-01 16:20:40 +0100

    tweak image loading - related to #10

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ import sys
 
 __author__ = """Wouter Verweirder"""
 __email__ = "wouter.verweirder@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .src.comfyui_sam3.nodes import NODE_CLASS_MAPPINGS
 from .src.comfyui_sam3.nodes import NODE_DISPLAY_NAME_MAPPINGS
